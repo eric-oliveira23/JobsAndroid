9d98c267677164671958024f5aac0691a9e013e6
Eric <ericpoliveira23@gmail.com>
2022-08-23 14:24:02 -0300

    Adiciona autenticação de usuário

--- app/src/main/java/com/eric/jobs/activities/LoginActivity.java
+++ app/src/main/java/com/eric/jobs/activities/LoginActivity.java
@@ -1,30 +1,105 @@
 package com.eric.jobs.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.eric.jobs.R;
+import com.eric.jobs.config.ConfigFirebase;
+import com.eric.jobs.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
+    private Usuario usuario;
+    private Button btnUserLogar;
+    private EditText edtEmail, edtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnUserLogar = findViewById(R.id.btnUserLogar);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+
+        btnUserLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = edtEmail.getText().toString();
+                String senha = edtSenha.getText().toString();
+
+                if (email.isEmpty() || senha.isEmpty()){
+                    Toast.makeText(LoginActivity.this,
+                            "Preencha os campos corretamente!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    usuario = new Usuario();
+                    usuario.setEmail(email);
+                    usuario.setSenha(senha);
+
+                    ValidarUser();
+                }
+
+            }
+        });
+
+    }
+
+    public void ValidarUser(){
+
+        autenticacao = ConfigFirebase.getAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+                else{
+
+                    String excecao;
+                    try{
+                        throw task.getException();
+                    }
+                    catch (FirebaseAuthInvalidCredentialsException e) {
+                        excecao = "Email e senha não correspondem a um usuário cadastrado!";
+                    }catch (FirebaseAuthInvalidUserException e){
+                        excecao = "Usuário não cadastrado!";
+                    }
+                    catch (Exception e) {
+                        excecao = "Erro ao cadastrar usuário! "+e.getMessage();
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(LoginActivity.this,
+                            excecao,
+                            Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
     }
 
     public void RegistrarClicked(View view){
         Intent intent = new Intent(this, CadastroActivity.class);
         startActivity(intent);
-    }
-
-    public void LogarClciked(View view){
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-        finish();
     }
 
 }
---
+++ app/src/main/java/com/eric/jobs/model/Usuario.java
@@ -0,0 +1,41 @@
+package com.eric.jobs.model;
+
+public class Usuario {
+
+    String nome;
+    String email;
+    String cidade;
+    String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
